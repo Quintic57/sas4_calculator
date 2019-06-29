99c8e036226e8673acb818f7a7cbe503ef276a1f
Destin <dwinata578@gmail.com>
2019-06-30 02:27:23 +0700

    CSV parsing should work now

--- C/src/sas4_calculator.c
+++ C/src/sas4_calculator.c
@@ -1,22 +1,103 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "sas4_calculator.h"
 
-Weapon assault_rifles[17];
-Weapon disc_throwers[1];
-Weapon flamethrowers[1];
-Weapon lasers[1];
-Weapon launchers[8];
-Weapon lmgs[12];
-Weapon pistols[11];
-Weapon shotguns[5];
-Weapon smgs[10];
-Weapon sniper_rifles[4];
+Weapon** weapons;
+Weapon* assault_rifles;
+Weapon* disc_throwers;
+Weapon* flame_throwers;
+Weapon* lasers;
+Weapon* lmgs;
+Weapon* pistols;
+Weapon* rocket_launchers;
+Weapon* shotguns;
+Weapon* smgs;
+Weapon* sniper_rifles;
 
 void init() {
+    /*Init the data structures*/
+    // Big daddy weapons array: 
+    weapons = calloc(NUM_WEAPON_CATEGORIES, sizeof(Weapon*));
+    weapons[ASSAULT_RIFLES] = assault_rifles;
+    weapons[DISC_THROWERS] = disc_throwers;
+    weapons[FLAME_THROWERS] = flame_throwers;
+    weapons[LASERS] = lasers;
+    weapons[LMGS] = lmgs;
+    weapons[PISTOLS] = pistols;
+    weapons[ROCKET_LAUNCHERS] = rocket_launchers;
+    weapons[SHOTGUNS] = shotguns;
+    weapons[SMGS] = smgs;
+    weapons[SNIPER_RIFLES] = sniper_rifles;
+
+    assault_rifles = calloc(NUM_ASSAULT_RIFLES, sizeof(Weapon));
+    disc_throwers = calloc(NUM_DISC_THROWERS, sizeof(Weapon));
+    flame_throwers = calloc(NUM_FLAME_THROWERS, sizeof(Weapon));
+    lasers = calloc(NUM_LASERS, sizeof(Weapon));
+    rocket_launchers = calloc(NUM_ROCKET_LAUNCHERS, sizeof(Weapon));
+    lmgs = calloc(NUM_LMGS, sizeof(Weapon));
+    pistols = calloc(NUM_PISTOLS, sizeof(Weapon));
+    shotguns = calloc(NUM_SHOTGUNS, sizeof(Weapon));
+    smgs = calloc(NUM_SMGS, sizeof(Weapon));
+    sniper_rifles = calloc(NUM_SNIPER_RIFLES, sizeof(Weapon));
+
     /*Parse the CSVs and get that juicy data*/
+    for (int category_num = 0; category_num < NUM_WEAPON_CATEGORIES; category_num++) {
+        char category_buf[64];
+
+        if (category_num == ASSAULT_RIFLES) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/assault_rifles.csv");
+        } else if (category_num == DISC_THROWERS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/disc_throwers.csv");
+        } else if (category_num == FLAME_THROWERS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/flamethrowers.csv");
+        } else if (category_num == LASERS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/lasers.csv");
+        } else if (category_num == LMGS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/lmgs.csv");
+        } else if (category_num == PISTOLS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/pistols.csv");
+        } else if (category_num == ROCKET_LAUNCHERS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/rocket_launchers.csv");
+        } else if (category_num == SHOTGUNS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/shotguns.csv");
+        } else if (category_num == SMGS) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/smgs.csv");
+        } else if (category_num == SNIPER_RIFLES) {
+            snprintf(category_buf, sizeof(category_buf), "%s",
+                "../data/sniper_rifles.csv");
+        }
+        // printf("%s\n", category_buf);
+        // FILE *fp = fopen(category_buf, "r");
+        // char buf[1024];
+
+        // if (!fp) {
+        //     printf("Can't open file\n");
+        //     return;
+        // }
+
+        // int i = 0;
+        // while (fgets(buf, 1024, fp)) {
+        //     if (i != 0) {
+        //         parse_csv_field(buf, assault_rifles, i);
+        //     }
+        //     i++;
+        // }
+
+        // fclose(fp);
+    }
+
     FILE *fp = fopen("../data/assault_rifles.csv", "r");
     char buf[1024];
 
@@ -28,38 +109,7 @@ void init() {
     int i = 0;
     while (fgets(buf, 1024, fp)) {
         if (i != 0) {
-            int attribute = 0;
-
-            // printf("%s\n", buf);
-            // printf("i is %i", i);
-            for (char* field = strtok(buf, ";"); field != NULL; field = strtok(NULL, ";")) {
-                // printf("%s\n", field);
-                char* temp = strdup(field);
-
-                if (attribute == NAME) {
-                    assault_rifles[i - 1].name = temp;
-                } else if (attribute == DMG) {
-                    double* dmg = parse_csv_array(temp);
-                    memcpy(assault_rifles[i - 1].dmg, dmg, sizeof(assault_rifles[i - 1].dmg));
-                    free(dmg);
-                } else if (attribute == PIERCE) {
-                    double* pierce = parse_csv_array(temp);
-                    memcpy(assault_rifles[i - 1].pierce, pierce, sizeof(assault_rifles[i - 1].pierce));
-                    free(pierce);
-                } else if (attribute == NUM_PELLETS) {
-                    sscanf(temp, "%hu", &assault_rifles[i - 1].num_pellets);
-                } else if (attribute == RATE_OF_FIRE) {
-                    
-                } else if (attribute == CAPACITY) {
-                    
-                } else if (attribute == RELOAD_TIME) {
-                    
-                } else {
-                    printf("%s\n", "Attribute is invalid");
-                }
-                // printf("Attribute #: %i\n", attribute);
-                attribute++;
-            }
+            parse_csv_field(buf, assault_rifles, i);
         }
         i++;
     }
@@ -67,9 +117,41 @@ void init() {
     fclose(fp);
 }
 
-// void parse_csv_field(char* row, Weapon* category) {
+void parse_csv_field(char* row, Weapon* category, int i) {
+    int attribute = 0;
 
-// }
+    // printf("%s\n", buf);
+    // printf("i is %i", i);
+    for (char* field = strtok(row, ";"); field != NULL; field = strtok(NULL, ";")) {
+        // printf("%s\n", field);
+        char* temp = strdup(field);
+
+        // TODO: Think I can clean up some of these
+        if (attribute == NAME) {
+            category[i - 1].name = temp;
+        } else if (attribute == DMG) {
+            double* dmg = parse_csv_array(temp);
+            memcpy(category[i - 1].dmg, dmg, sizeof(category[i - 1].dmg));
+            free(dmg);
+        } else if (attribute == PIERCE) {
+            double* pierce = parse_csv_array(temp);
+            memcpy(category[i - 1].pierce, pierce, sizeof(category[i - 1].pierce));
+            free(pierce);
+        } else if (attribute == NUM_PELLETS) {
+            sscanf(temp, "%hu", &category[i - 1].num_pellets);
+        } else if (attribute == RATE_OF_FIRE) {
+            parse_csv_string(temp, &category[i - 1].rate_of_fire);
+        } else if (attribute == CAPACITY) {
+            sscanf(temp, "%hu", &category[i - 1].capacity);
+        } else if (attribute == RELOAD_TIME) {
+            parse_csv_string(temp, &category[i - 1].reload_time);
+        } else {
+            printf("%s\n", "Attribute is invalid");
+        }
+        // printf("Attribute #: %i\n", attribute);
+        attribute++;
+    }
+}
 
 double* parse_csv_array(char* str) {
     char* end_char = strchr(str, ']');
@@ -108,6 +190,19 @@ double* parse_csv_array(char* str) {
     return values;
 }
 
+void parse_csv_string(char* str, double* value) {
+    bool done = false;
+    char* temp;
+    for (int i = 0; !done && i < sizeof(str); i++) {
+        if (str[i] == ' ') {
+            memcpy(temp, str, i);
+            done = true;
+        }
+    }
+
+    sscanf(temp, "%lf", value);
+}
+
 int main() {
     init();
 
@@ -124,5 +219,12 @@ int main() {
     }
     // NUM_PELLETS
     printf("%hu\n", assault_rifles[1].num_pellets);
+    // ROF
+    printf("%lf\n", assault_rifles[1].rate_of_fire);
+    // Capacity
+    printf("%hu\n", assault_rifles[1].capacity);
+    // Reload Time
+    printf("%lf\n", assault_rifles[1].reload_time);
+
     return 0;
 }
--- C/src/sas4_calculator.h
+++ C/src/sas4_calculator.h
@@ -3,10 +3,33 @@ typedef struct Weapon {
 	double dmg[3];
     double pierce[3];
     unsigned short int num_pellets;
-	unsigned int rate_of_fire;
+	double rate_of_fire;
     unsigned short int capacity;
     double reload_time;
 } Weapon;
+
+#define NUM_WEAPON_CATEGORIES 10
+#define NUM_ASSAULT_RIFLES 17
+#define NUM_DISC_THROWERS 1
+#define NUM_FLAME_THROWERS 1
+#define NUM_LASERS 1
+#define NUM_LMGS 12
+#define NUM_PISTOLS 11
+#define NUM_ROCKET_LAUNCHERS 8
+#define NUM_SHOTGUNS 5
+#define NUM_SMGS 10
+#define NUM_SNIPER_RIFLES 4
+
+#define ASSAULT_RIFLES 0
+#define DISC_THROWERS 1
+#define FLAME_THROWERS 2
+#define LASERS 3
+#define LMGS 4
+#define PISTOLS 5
+#define ROCKET_LAUNCHERS 6
+#define SHOTGUNS 7
+#define SMGS 8
+#define SNIPER_RIFLES 9
 
 #define NORMAL 0
 #define RED 1
@@ -22,3 +45,5 @@ typedef struct Weapon {
 
 void init();
 double* parse_csv_array(char* str);
+void parse_csv_field(char* row, Weapon* category, int i);
+void parse_csv_string(char* str, double* value);
